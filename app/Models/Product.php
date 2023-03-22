4943f7e96b537aa73620a65db534cc6c0d53090a
FiqhroDeden <dedhensupatmo@gmail.com>
2023-03-22 09:14:37 +0900

    Add model and migration for products

---
+++ app/Models/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $fillable = ['name', 'code', 'brand_id', 'quantity', 'unit_id', 'unit_value', 'category_id', 'subcategory_id', 'selling_price', 'purchase_price', 'discount_type', 'discount_value', 'tax', 'supplier_id', 'image'];
+}
